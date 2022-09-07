cf2ff8093e0238cc277bc9d7e64f03789063344c
Alyaa <alyaa.moh.bakr@gmail.com>
2022-09-08 01:31:05 +0200

    Comments Update

--- Titanic_Survival_Predictions_ML.py
+++ Titanic_Survival_Predictions_ML.py
@@ -4,6 +4,7 @@
 # In[1]:
 
 
+## Import packages
 import pandas as pd
 from pandas import set_option
 import matplotlib.pyplot as plt
@@ -47,6 +48,7 @@ rcParams['ytick.major.pad'] = 1
 # In[2]:
 
 
+# Load data from a csv file
 Data = pd.read_csv('train.csv')
 Data.head()
 
@@ -54,6 +56,7 @@ Data.head()
 # In[3]:
 
 
+# Drop unecessary columns
 Data.drop(['PassengerId','Name','Embarked'], axis = 1, inplace = True)
 Data.head(10)
 
@@ -61,6 +64,7 @@ Data.head(10)
 # In[4]:
 
 
+# Drop unecessary columns
 Data.drop(['Ticket','Fare','Cabin'], axis = 1, inplace = True)
 Data.head(10)
 
@@ -68,6 +72,7 @@ Data.head(10)
 # In[5]:
 
 
+# Numerize Survived "male" malignant; "female" benign using a dictionary and map function
 Sex_coder = {'male':1, 'female':0}
 Data.Sex = Data.Sex.map(Sex_coder)
 Data.head()
@@ -76,6 +81,7 @@ Data.head()
 # In[6]:
 
 
+# Reorder columsn so diagnosis is right-most
 Survived = Data.Survived
 Data.drop('Survived', axis = 1, inplace = True)
 Data['Survived'] = Survived
@@ -98,6 +104,7 @@ Data.groupby('Survived').mean()
 # In[8]:
 
 
+# Create list of features related to the mean
 Data_d = Data[Data['Survived'] == 0]
 Data_s = Data[Data['Survived'] == 1]
 features_means =list(Data.columns[0:10])
@@ -110,6 +117,7 @@ print(Data.columns[0:10])
 # In[9]:
 
 
+#calculate how many survived or died
 outcome_count = Data.Survived.value_counts()
 outcome_count = pd.Series(outcome_count)
 outcome_count = pd.DataFrame(outcome_count)
@@ -123,6 +131,7 @@ outcome_count
 # In[10]:
 
 
+# Visualize frequency of alive/dead in dataset
 sns.barplot(x = ['alive', 'dead'], y = 'Survived', data = outcome_count, alpha = .8)
 plt.title('Frequency of Survived Outcomes in Dataset')
 plt.ylabel('Frequency')
@@ -132,6 +141,7 @@ plt.show()
 # In[11]:
 
 
+# Visualize Percentage of alive/dead in dataset
 sns.barplot(x = ['alive', 'dead'], y = 'Percent', data = outcome_count, alpha = .8)
 plt.title('Percentage of Survived Outcomes in Dataset')
 plt.ylabel('Percentage')
@@ -142,7 +152,10 @@ plt.show()
 # In[12]:
 
 
+# Instantiate a figure object for OOP figure manipulation.
 fig = plt.figure(figsize=(10,20))
+
+# Create 'for loop' to enerate though Data features and compare with histograms
 for i,b in enumerate(list(Data.columns[0:5])):
     
     i +=1
@@ -162,7 +175,10 @@ plt.show()
 # In[13]:
 
 
+# Instantiate figure object
 fig = plt.figure(figsize=(30,20))
+
+# Create 'for loop' to enerate though Data features and compare with histograms
 for i,b in enumerate(list(Data.columns[0:5])):
     
     i +=1
@@ -183,6 +199,8 @@ plt.show()
 
 
 fig = plt.figure(figsize=(20,10))
+
+# Create 'for loop' to enerate though Data features and compare with histograms
 for i,b in enumerate(list(Data.columns[0:5])):
     i +=1
     
@@ -200,6 +218,7 @@ plt.show()
 # In[15]:
 
 
+# Quick visualization of relationships between features and diagnoses
 sns.heatmap(Data.corr())
 sns.set_style("whitegrid")
 plt.show()
@@ -237,8 +256,13 @@ y_test.head()
 
 X_train=Data.iloc[:,:-1].values
 y_train=Data['Survived'].values
+
+# The normalize features to account for feature scaling
+# Instantiate 
 norm = Normalizer()
 norm.fit(X_train)
+
+# Transform both training and testing sets
 X_train_norm = norm.transform(X_train)
 X_test_norm = norm.transform(X_test)
 
@@ -246,6 +270,11 @@ X_test_norm = norm.transform(X_test)
 # In[20]:
 
 
+# ## Model Testing
+# * We'll start by testing a variety of algorithms using scikit-learns's gridsearch method for model optimization. 
+# * Our models will include both parametric (e.g. SVM) and non-parametric (e.g. KNN) and linear (e.g. Logistic Regression) and nonlinear modles (e.g. Random Forest Classifier).
+
+# Define parameters for optimization using dictionaries {parameter name: parameter list}
 SVM_params = {'C':[0.001, 0.1, 10, 100], 'kernel':['rbf' ,'linear', 'poly', 'sigmoid']}
 LR_params = {'C':[0.001, 0.1, 1, 10, 100]}
 LDA_params = {'n_components':[None, 1,2,3], 'solver':['svd'], 'shrinkage':[None]}
@@ -257,6 +286,7 @@ DTC_params = {'criterion':['entropy', 'gini'], 'max_depth':[10, 50, 100]}
 # In[21]:
 
 
+# Append list of models with parameter dictionaries
 models_opt = []
 
 models_opt.append(('LR', LogisticRegression(), LR_params))
@@ -303,12 +333,19 @@ estimator_function(models_opt, scoring = 'accuracy')
 # In[24]:
 
 
+# Guassian Naive Bayes does not require optimization so we will run it separately without
+# gridsearch and append the performance results to the results and names lists.
+
+# Instantiate model
 GNB =  GaussianNB()
- 
+
+# Define kfold - this was done above but not as a global variable
 kfold = KFold(len(X_train_norm), random_state=2, shuffle=True)
 
+# Run cross validation
 cv_results_GNB= cross_val_score(GNB,X_train_norm, y_train, cv = kfold )
 
+# Append results and names lists
 results.append(cv_results_GNB)
 names.append('GNB')
 
@@ -316,11 +353,15 @@ names.append('GNB')
 # In[25]:
 
 
+# Ensemble Voting
 from sklearn.ensemble import VotingClassifier
 
+# Create list for estimatators
 estimators = []
+# Create estimator object
 model1 = LogisticRegression()
 
+# Append list with estimator name and object
 estimators.append(("logistic", model1))
 model2 = DecisionTreeClassifier()
 estimators.append(("cart", model2))
@@ -350,6 +391,8 @@ print('Accuracy: {} SD: {}'.format(results_voting.mean(), results_voting.std()))
 # In[26]:
 
 
+# Visualize model accuracies for comparision - boxplots will be appropriate to visualize 
+# data variation
 plt.boxplot(results, labels = names)
 plt.title('Titanic survivals Accuracy using Various Machine Learning Models')
 plt.ylabel('Model Accuracy %')
@@ -358,68 +401,45 @@ plt.ylim(1,1)
 plt.show()
 
 
-# In[27]:
-
-
-lda_2 = LinearDiscriminantAnalysis()
-
-lda_2.fit(X_train_norm, y_train)
-
-lda_2_predicted = lda_2.predict(X_test_norm)
-
-
-# In[28]:
-
-
-print('Linear discriminant model analyis Accuracy is: {}'.format(accuracy_score(y_test,lda_2_predicted )))
-
-
-# In[29]:
-
-
-confusion_matrix_lda = pd.DataFrame(confusion_matrix(y_test, lda_2_predicted), index = ['Actual Negative','Actual Positive'], columns = ['Predicted Negative','Predicted Postive'] )
-
-print('Linear discriminant Model Confusion Matrix')
-confusion_matrix_lda
-
-
-# In[30]:
-
-
-print('Linear discriminant Model Classification Report')
-print(classification_report(y_test, lda_2_predicted))
-
-
 # In[31]:
 
 
+# ### Follow-up Model Testing
+# * retry this model on test data after training on the entire training set.
+
+# #### Test Random Forest Classifier
 RF_params = {'n_estimators':[10,50,100, 200]}
 
 RFC_2 = RandomForestClassifier(random_state=42)
 
+# Instantiate gridsearch using RFC model and dictated parameters
 RFC_2_grid = GridSearchCV(RFC_2, RF_params)
 
+# Fit model to training data
 RFC_2_grid.fit(X_train_norm, y_train)
 
 print('Optimized number of estimators: {}'.format(RFC_2_grid.best_params_.values()))
 
 
-# In[32]:
+# In[34]:
 
 
+# Train RFC on whole training set
+
+# Instantiate RFC with optimal parameters
 RFC_3 = RandomForestClassifier(n_estimators=50, random_state=42)
 
 RFC_3.fit(X_train_norm, y_train)
 
+# Predict on training data using fitted RFC
+
+# Evalaute RFC with test data
 RFC_3_predicted = RFC_3.predict(X_test_norm)
-print('Model accuracy on test data: {}'.format(accuracy_score(y_test, RFC_3_predicted)))
 
-
-# In[34]:
-
-
+# Create dataframe by zipping RFC feature importances and column names
 rfc_features = pd.DataFrame(list(zip(RFC_3.feature_importances_, Data.columns[:-1])), columns = ['Importance', 'Features'])
 
+# Sort in descending order for easy organization and visualization
 rfc_features = rfc_features.sort_values(['Importance'], ascending=False)
 
 
@@ -435,14 +455,23 @@ plt.show()
 # In[36]:
 
 
+# ### Features Selection with Logistic Regression and Recursive Feature Elimination
+
+# Instantiate new logistic regression for use with scikit-learn's recursive feature elimination...
+# with cross validation (RFECV)
 lr_2 = LogisticRegression()
+
+# Instantiate RFECV with logistic regression classifier
 selector = RFECV(lr_2, cv = 10, scoring='accuracy')
+
+# Fit RFECV to training data
 selector.fit(X_train_norm, y_train)
 
 
 # In[37]:
 
 
+#Plot number of features VS. cross-validation scores
 plt.figure()
 plt.xlabel("Number of features selected")
 plt.ylabel("Model Accuracy %")
@@ -463,10 +492,19 @@ plt.show()
 # In[38]:
 
 
+# ## Dimensional Reduction
+
+# Use dimensional reduction to reduce our 30 features into principal components (PCA) that maximally explain the data variance. While other forms of dimensional reduction exist (factor analysis, LDA, etc.), PCA is common approach and worth exploring with this data set.
+
+# #### Evaluation of Variance with PCA Component Number
+
+# Instantiate PCA
 pca_var = PCA()
 
+# Fit PCA to training data
 pca_var.fit(X_train_norm)
 
+# Visualize explained variance with an increasing number of components
 plt.plot(pca_var.explained_variance_, 'bo-', markersize=8)
 plt.title("Elbow Curve for PCA Dimension of Titanic survivals Data")
 plt.ylabel('Explained Variance')
@@ -478,15 +516,22 @@ plt.show()
 # In[39]:
 
 
+# #### Visualization data by PCA  - 3D
+
+#Instantiate new PCA object
 pca = PCA(n_components=3)
 
+# Fit and transform training data with PCA using 3 components
 pca.fit(X_train_norm)
 X_train_norm_pca = pca.transform(X_train_norm)
 
+# Create a dataframe of 3 PCA
 pca_df = pd.DataFrame(X_train_norm_pca, columns = ['PCA1', 'PCA2', 'PCA3'])
 
+# Append diagnosis data into PCA dataframe
 pca_df['Survived'] = y_train
 
+# Visualize PCA in a 3D plot - color points by diagnsosis to see if a visuale stratification occurs
 pca_fig = plt.figure().gca(projection = '3d')
 pca_fig.scatter(pca_df['PCA1'], pca_df['PCA2'], pca_df['PCA3'], c = pca_df['Survived'], cmap=cm.coolwarm)
 pca_fig.set_xlabel('PCA1')
@@ -502,26 +547,34 @@ plt.show()
 # In[40]:
 
 
+# Next, test logistic regression performance using PCA and scikit-learn's pipeline method.
 
+# Create features list to use to instantiate the FeatureUnion
 features = []
 
+# Append features list
 features.append(('pca', PCA(n_components=3)))
 features.append(('select_best', SelectKBest(k='all')))
 
+# Instantiate FeatureUnion object
 feature_union = FeatureUnion(features)
 
+# Create pipeline using esimator list, append with feature union and logistic regression
 estimators = []
 estimators.append(('feature_union', feature_union))
 estimators.append(('logistic', LogisticRegression()))
 
+# Instantiate model using pipeline method
 model_feature_union = Pipeline(estimators)
 
+# Evaluate Pipeline model performance using cross validation
 results_feature_union = cross_val_score(model_feature_union, X_train_norm, y_train, cv=kfold)
 
 
 # In[41]:
 
 
+# Append results and names lists
 results.append(results_feature_union)
 names.append('LR-PCA')
 
